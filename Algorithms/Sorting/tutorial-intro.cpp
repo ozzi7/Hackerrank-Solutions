08623d222c0b12e622a0c4ac84c79d0bf0b498f4
ozzi7 <nielregen@gmail.com>
2017-09-16 20:46:12 +0200

    Update

---
+++ Algorithms/Sorting/tutorial-intro.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <stack>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <numeric>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define pii pair <int, int>
+#define pll pair <long long, long long>
+#define vi vector <int>
+
+#define FOR(i, a, b) for (int i=a; i<b; i++)
+#define F0R(i, a) for (int i=0; i<a; i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+
+const int MOD = 1000000007;
+
+long long gcd(long long a, long long b) { if (b == 0) return a; a %= b; return gcd(b, a); }
+long long lcm(long long a, long long b) { return (a * b / gcd(a, b)); }
+
+struct node {
+	int parent;
+	vector<int> children;
+};
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	int sample; cin >> sample;
+	int n; cin >> n;
+	int count = 0;
+	F0R(i, n) {
+		int s;
+		cin >> s;
+		if (s == sample) cout << count << endl;
+		count++;
+	}
+	return 0;
+}
