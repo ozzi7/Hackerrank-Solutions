3334903241f5137c8e45ba8d7fbc7da4b66cc10e
Pascal Widmer <pawiddy@gmail.com>
2017-10-16 23:01:46 +0200

    Update

---
+++ Artificial Intelligence/Bot Building/botcleanlarge.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+void next_move(int posr, int posc, vector <string> board) {
+	pair<int, int> bot = make_pair(posr, posc);
+	pair<int, int> nextClean; vector <string> copy = board; vector <string> copy2 = copy;
+	int radius = 0;
+	bool searchPhase = true;
+	
+	if (copy2[posr][posc] == 'd') {
+		cout << "CLEAN" << endl;
+		return;
+	}
+	copy2[posr][posc] = 'v';
+	while (searchPhase) {
+		copy = copy2;
+		for (int i = 0; searchPhase && i < board.size(); ++i) {
+			for (int j = 0; searchPhase && j < board[0].size(); ++j) {
+				if (copy[i][j] == 'v') {
+					if (i - 1 >= 0) {
+						if (copy[i - 1][j] == 'd') {
+							nextClean = make_pair(i - 1, j);
+							searchPhase = false;
+							break;
+						}
+						else copy2[i - 1][j] = 'v';
+					}
+					if (j - 1 >= 0) {
+						if (copy[i][j - 1] == 'd') {
+							nextClean = make_pair(i, j - 1);
+							searchPhase = false;
+							break;
+						}
+						else copy2[i ][j-1] = 'v';
+					}					
+					if (i + 1 < board.size()) {
+						if (copy[i + 1][j] == 'd') {
+							nextClean = make_pair(i + 1, j);
+							searchPhase = false;
+							break;
+						}
+						else copy2[i + 1][j] = 'v';
+					}
+					if (j + 1 < board[0].size()) {
+						if (copy[i][j + 1] == 'd') {
+							nextClean = make_pair(i, j + 1);
+							searchPhase = false;
+							break;
+						}
+						else copy2[i][j+1] = 'v';
+					}
+				}
+			}
+		}
+	}
+
+	if (bot.first > nextClean.first) {
+		cout << "UP" << endl;
+		bot.first--;
+	}
+	else if (bot.first < nextClean.first) {
+		cout << "DOWN" << endl;
+		bot.first++;
+	}
+	else if (bot.second > nextClean.second) {
+		cout << "LEFT" << endl;
+		bot.second--;
+	}
+	else if (bot.second < nextClean.second) {
+		cout << "RIGHT" << endl;
+		bot.second++;
+	}
+	else {
+		cout << "CLEAN" << endl;
+	}
+}
+
+int main(void) {
+    int pos[2];
+    int dim[2];
+    vector <string> board;
+    cin>>pos[0]>>pos[1];
+    cin>>dim[0]>>dim[1];
+    for(int i=0;i<dim[0];i++) {
+        string s;cin >> s;
+        board.push_back(s);
+    }
+    next_move(pos[0], pos[1], board);
+    return 0;
+}
---
+++ Artificial Intelligence/Statistics and Machine Learning/correlation-and-regression-lines-8.py
@@ -0,0 +1,13 @@
+# import numpy as np
+# from sklearn import linear_model
+# 
+# phys = [15.0,12.0,8.0,8.0,7.0,7.0,7.0,6.0,5.0,3.0]
+# hist = [10.0,25.0,17.0,11.0,13.0,17.0,20.0,13.0,9.0,15.0]
+# 
+# Anp = np.reshape(phys, (-1,1))
+# Bnp =  np.reshape(hist, (-1,1))
+# linear_model = linear_model.LinearRegression(fit_intercept=True, n_jobs=1, normalize=False)
+# linear_model.fit(Anp,Bnp)
+# prediction = linear_model.predict(10.0)
+
+print(15.45) # prediction variable is 15.45, libraries are all disallowed
